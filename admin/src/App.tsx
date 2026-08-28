739f1a22acbc5e4a837955e3c0b157297d6fa6fd
aan bahudin <aan.bahudin11@gmail.com>
2026-08-28 22:06:19 +0700

    setting up project

---
+++ admin/src/App.tsx
@@ -0,0 +1,12 @@
+import {Button} from '@/components/ui/button'
+
+const App = () => {
+  return (
+    <div className='w-full'>
+      <Button variant='destructive' size='lg'>Hapus Barang</Button>
+    </div>
+
+  )
+}
+
+export default App
